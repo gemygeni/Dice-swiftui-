cb046d7bfc13a9b90fb98d3cc8d10a35a591de5c
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-03-08 22:30:15 +0200

    Initial Commit

---
+++ Dice(swiftui)/Dice_swiftui_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_swiftui_App.swift
+//  Dice(swiftui)
+//
+//  Created by AHMED GAMAL  on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dice_swiftui_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
